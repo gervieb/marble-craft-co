32e7f2d034400b9a58cc1487676ec1248c4e3dc4
gervieb <gerviebarczyk@gmail.com>
2023-12-15 02:56:06 +0100

    change images

--- src/app/(global)/home/HomeCategories.tsx
+++ src/app/(global)/home/HomeCategories.tsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import { Box, Typography, Grid } from '@mui/material'
-import Image from 'next/image'
-import { homeCategories } from '@/_mock/_categories'
+import React from 'react';
+import { Box, Typography, Grid } from '@mui/material';
+import Image from 'next/image';
+import { homeCategories } from '@/_mock/_categories';
 
 const HomeCategories = () => {
+  return (
+    <Box>
+      <Grid
+        container
+        spacing={5}
+        mt={2}
+        sx={{
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {homeCategories.map((product) => (
+          <Grid item key={product.id} sx={{ width: '238px', mt: 2 }} xs={3}>
+            <Image
+              src={product.url}
+              alt="christmas marble product"
+              width="0"
+              height="0"
+              sizes="100vw"
+              style={{
+                maxWidth: '100%',
+                height: '300px',
+                padding: 24,
+                // backgroundColor: '#9E9FA5',
+              }}
+            />
+            <Typography sx={{ textAlign: 'center' }}>{product.name}</Typography>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
 
-    return (
-        <Box>
-            <Grid container spacing={5} mt={2} sx={{
-                alignItems: 'center',
-                justifyContent: 'center'
-            }}>
-                {homeCategories.map(product => (
-                    <Grid item key={product.id} sx={{ width: '170px', mt: 2, }} xs={3}>
-                        <Image src={product.url} alt='christmas marble product' width="0"
-                            height="0"
-                            sizes="100vw"
-                            style={{ width: '100%', height: '300px', padding: 24, backgroundColor: '#9E9FA5' }}
-                        />
-                        <Typography sx={{ textAlign: 'center' }}>{product.name}</Typography>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    )
-}
-
-export default HomeCategories
+export default HomeCategories;
