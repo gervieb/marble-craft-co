8b026cfafcd26be610caf18ead7350b48e3fbaf1
gervieb <gerviebarczyk@gmail.com>
2023-12-29 19:49:27 +0100

    update styling

--- src/app/(global)/shop/page.tsx
+++ src/app/(global)/shop/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Grid, Box, Typography, Stack, Button } from '@mui/material';
+import { Grid, Box, Typography, Container } from '@mui/material';
 import React from 'react';
 import { productList } from '@/_mock/_products';
 import Image from 'next/image';
@@ -9,21 +9,22 @@ import PageLayout from '@/app/layout/PageLayout';
 const Collection = () => {
   return (
     <PageLayout>
-      <Grid container spacing={6}>
-        {productList.map((product) => (
-          <Grid item key={product.id} sx={{ my: 4 }} xs>
-            <Box sx={{ width: '230px' }}>
-              <Image
-                src={product.url}
-                alt="christmas marble product"
-                width="0"
-                height="0"
-                sizes="100vw"
-                style={{ width: 'inherit', height: 'inherit' }}
-              />
-              <Box>
-                <Typography>{product.name}</Typography>
-                {/* <Stack
+      <Container maxWidth="lg">
+        <Grid container spacing={2} mt={1}>
+          {productList.map((product) => (
+            <Grid item key={product.id} sx={{ my: 4 }} xs>
+              <Box sx={{ width: '230px' }}>
+                <Image
+                  src={product.url}
+                  alt="christmas marble product"
+                  width="0"
+                  height="0"
+                  sizes="100vw"
+                  style={{ width: 'inherit', height: 'inherit' }}
+                />
+                <Box>
+                  <Typography>{product.name}</Typography>
+                  {/* <Stack
                   direction="row"
                   display="flex"
                   alignItems="center"
@@ -32,11 +33,12 @@ const Collection = () => {
                   <Typography>Php {product.price}</Typography>
                   <Button variant="text">Add to cart</Button>
                 </Stack> */}
+                </Box>
               </Box>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </PageLayout>
   );
 };
