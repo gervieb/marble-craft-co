2e53a99261eb46f114f18d9f73c10ed5615aae73
gervieb <gerviebarczyk@gmail.com>
2024-01-24 02:51:23 +0100

    fix homepage background

--- src/app/(global)/home/page.tsx
+++ src/app/(global)/home/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import PageLayout from '@/app/layout/PageLayout';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Stack } from '@mui/material';
 import NewCollection from './NewCollection';
 import BestSeller from '@/app/(global)/home/BestSeller';
 import HomeCategories from '@/app/(global)/home/HomeCategories';
@@ -14,88 +14,101 @@ const HomePage = () => {
   return (
     <PageLayout>
       <Box>
-        <Box
+        <Stack
           mt={6}
           sx={{
             display: 'flex',
             flexDirection: 'row',
-            justifyContent: 'space-around',
-            alignItems: 'flex-start',
+            justifyContent: 'space-between',
+            alignItems: 'center',
             height: '400px',
-            backgroundImage: `url('/assets/home-background.jpg')`,
+            backgroundColor: '#c2c5ca',
           }}
         >
           <Box>
-            <Box mt={18}>
-              <Typography
-                sx={{
-                  fontSize: 32,
-                  fontFamily: courgette.style.fontFamily,
-                }}
-              >
-                Discover the{' '}
-                <span
-                  style={{
-                    color: '#000',
-                    fontWeight: 600,
-                  }}
-                >
-                  beauty
-                </span>{' '}
-              </Typography>
-              <Typography
-                sx={{
-                  fontSize: 28,
-                  fontFamily: courgette.style.fontFamily,
-                }}
-              >
-                of handcrafted{' '}
-                <span
-                  style={{
-                    color: '#000',
-                    fontWeight: 600,
-                  }}
-                >
-                  marble
-                </span>
-              </Typography>
-            </Box>
-            <Link
-              href="/shop"
-              style={{
-                textDecoration: 'none',
+            <Box
+              sx={{
+                position: 'absolute',
+                top: '55%',
+                left: '25%',
+                transform: 'translateX(-50%) translateY(-50%)',
               }}
             >
-              <Typography
-                sx={{
-                  cursor: 'pointer',
-                  mt: 5,
-                  padding: '2px 14px',
-                  border: '1px solid #747334',
-                  borderRadius: '4px',
-                  width: 'fit-content',
-
-                  '&:hover': {
-                    backgroundColor: '#747334',
-                    color: '#fff',
-                  },
+              <Box>
+                <Typography
+                  sx={{
+                    fontSize: 32,
+                    fontFamily: courgette.style.fontFamily,
+                  }}
+                >
+                  Discover the{' '}
+                  <span
+                    style={{
+                      color: '#000',
+                      fontWeight: 600,
+                    }}
+                  >
+                    beauty
+                  </span>{' '}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: 28,
+                    fontFamily: courgette.style.fontFamily,
+                  }}
+                >
+                  of handcrafted{' '}
+                  <span
+                    style={{
+                      color: '#000',
+                      fontWeight: 600,
+                    }}
+                  >
+                    marble
+                  </span>
+                </Typography>
+              </Box>
+              <Link
+                href="/shop"
+                style={{
+                  textDecoration: 'none',
                 }}
               >
-                See Gallery
-              </Typography>
-            </Link>
+                <Typography
+                  sx={{
+                    cursor: 'pointer',
+                    mt: 5,
+                    padding: '2px 14px',
+                    border: '1px solid #747334',
+                    borderRadius: '4px',
+                    width: 'fit-content',
+
+                    '&:hover': {
+                      backgroundColor: '#747334',
+                      color: '#fff',
+                    },
+                  }}
+                >
+                  See Gallery
+                </Typography>
+              </Link>
+            </Box>
           </Box>
-          <Box sx={{ alignItems: 'right' }}>
+          <Box sx={{ height: 'inherit' }}>
             <Image
               src={'/assets/home-background.jpg'}
               alt="christmas marble product"
               width="0"
               height="0"
               sizes="100vw"
-              style={{ width: '100%', height: '400px' }}
+              style={{
+                width: 'auto',
+                height: 'inherit',
+                boxShadow: '10px 21px 46px -59px rgba(66, 68, 90, 1)',
+              }}
             />
           </Box>
-        </Box>
+        </Stack>
 
         <Box sx={{ mt: 6 }}>
           <HomeCategories />
