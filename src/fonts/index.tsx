e9fe871cfd75f4880b17bfd1fc4dfce289af34ec
gervieb <gerviebarczyk@gmail.com>
2024-03-09 17:08:03 +0100

    remove bodoni font

--- src/fonts/index.tsx
+++ src/fonts/index.tsx
@@ -2,33 +2,33 @@ import {
   Jost,
   Alex_Brush,
   Roboto,
-  Bodoni_Moda,
+  // Bodoni_Moda,
   Courgette,
-} from 'next/font/google';
+} from "next/font/google";
 
 export const jost = Jost({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-josh',
-  fallback: ['Roboto'],
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-josh",
+  fallback: ["Roboto"],
 });
 
 export const roboto = Roboto({
-  subsets: ['latin'],
-  weight: '400',
+  subsets: ["latin"],
+  weight: "400",
 });
 
-export const bodoniModa = Bodoni_Moda({
-  subsets: ['latin'],
-  weight: '400',
-});
+// export const bodoniModa = Bodoni_Moda({
+//   subsets: ['latin'],
+//   weight: '400',
+// });
 
 export const alexBrush = Alex_Brush({
-  subsets: ['latin'],
-  weight: '400',
+  subsets: ["latin"],
+  weight: "400",
 });
 
 export const courgette = Courgette({
-  subsets: ['latin'],
-  weight: '400',
+  subsets: ["latin"],
+  weight: "400",
 });
