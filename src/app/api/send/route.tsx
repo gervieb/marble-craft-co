befabd29c6b7dbb0d26bb5c6cc8204c330fff3d6
gervieb <gerviebarczyk@gmail.com>
2024-03-09 20:43:07 +0100

    fix build error

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,15 +1,15 @@
-import TestEmail from '@/app/email/test/page';
-import { Resend } from 'resend';
+import TestEmail from "@/app/email/test/page";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   try {
     const data = await resend.emails.send({
-      from: `Marble Craft Co. <${process.env.FROM_EMAIL}>`,
+      from: `${process.env.COMPANY_NAME} <${process.env.FROM_EMAIL}>`,
       to: `${process.env.TO_EMAIL}`,
-      subject: 'Hello world',
-      react: TestEmail({ name: 'John Doe' }),
+      subject: "Hello world",
+      react: <TestEmail name="John Doe" />,
     });
 
     return Response.json(data);
--- src/app/email/test/page.tsx
+++ src/app/email/test/page.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-
 import { Button, Html } from "@react-email/components";
 
 interface ITestEmail {
   name: string;
 }
 
-const TestEmail = ({ name }: ITestEmail) => {
+export const TestEmail = ({ name }: ITestEmail) => {
   return (
     <Html>
       <Button
-        href="https://www.marblecraftco.com"
+        href={process.env.WEBSITE}
         style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
       >
         Click me
